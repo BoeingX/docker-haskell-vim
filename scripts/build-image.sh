85dad7b205d9ae1d22f29456360f52d339cffa05
BoeingX <song.baoyang@gmail.com>
2018-09-07 22:34:23 +0200

    Add custom build script which build docker image only if the docker folder changes

---
+++ scripts/build-image.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ev
+target=docker/
+image=haskell-vim
+if [[ $(git --no-pager diff HEAD~1..HEAD "$target") ]]; then
+    echo "Changes detected in $target"
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+    cd docker
+    echo "Build docker image"
+    docker build -t "$image" .
+    echo "Tag image"
+    docker tag haskell-vim "$DOCKER_USERNAME/$image"
+    echo "Push to docker hub"
+    docker push "$DOCKER_USERNAME/$image"
+else
+    echo "No file changed in ${target}, skip build"
+fi
